4832d502087358e392ee97ee6fbb1bdca4bb6216
Alejandro Cardenas <alecar.main@gmail.com>
2020-11-17 14:07:46 -0500

    Adding username patterns allowed to validate username in registration form

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -48,7 +48,7 @@ import lms.envs.common
 # Although this module itself may not use these imported variables, other dependent modules may.
 from lms.envs.common import (
     USE_TZ, ALL_LANGUAGES, update_module_store_settings, ASSET_IGNORE_REGEX,
-    PARENTAL_CONSENT_AGE_LIMIT, REGISTRATION_EMAIL_PATTERNS_ALLOWED,
+    PARENTAL_CONSENT_AGE_LIMIT, REGISTRATION_EMAIL_PATTERNS_ALLOWED, REGISTRATION_USERNAME_PATTERNS_ALLOWED,
     # The following PROFILE_IMAGE_* settings are included as they are
     # indirectly accessed through the email opt-in API, which is
     # technically accessible through the CMS via legacy URLs.
