48f2090090d6a5f9e97acb628c51368147972554
mariagrimaldi <maria.grimaldi@edunext.co>
2021-03-24 15:40:23 -0400

    Update hooks configuration name

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -432,7 +432,7 @@ FEATURES['ENABLE_PREREQUISITE_COURSES'] = True
 # Used in edx-proctoring for ID generation in lieu of SECRET_KEY - dummy value
 # (ref MST-637)
 PROCTORING_USER_OBFUSCATION_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
-HOOKS_EXTENSIONS_CONFIG = {
+HOOKS_EXTENSION_CONFIG = {
       "openedx.lms.auth.post_register.action.v1": {
                "pipeline": [
                      "openedx_basic_hooks.actions.post_register.post_register_tweet",
