21a8712b72b10eb03b30f8be55d681cd675c5db2
Eric Herrera <eric.herrera@edunext.co>
2021-08-01 09:22:31 -0500

    fix: Improve and fix codejail service integration
    Address recommendations and fixes discussed in PR.
    Use similar structuctre to notes app.

---
+++ common/lib/capa/capa/safe_exec/exceptions.py
@@ -0,0 +1,9 @@
+"""
+Exceptions related to safe exec.
+"""
+
+class CodejailServiceUnavailable(Exception):
+    """
+    An exception that is raised whenever Codejail service is unavailable.
+    """
+    pass  # lint-amnesty, pylint: disable=unnecessary-pass
