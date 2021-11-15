e72353aad9dda84b743f0bac34a5e25ef2d90060
Maria Grimaldi <maria.grimaldi@edunext.co>
2021-11-15 17:04:50 -0400

    add tests for filters

---
+++ common/djangoapps/student/tests/test_filters.py
@@ -0,0 +1,92 @@
+"""
+Test that various filters are fired for models in the student app.
+"""
+from unittest import mock
+import pytest
+
+from django.db.utils import IntegrityError
+from django.test import TestCase
+from django_countries.fields import Country
+
+from common.djangoapps.student.models import CourseEnrollment
+from common.djangoapps.student.tests.factories import UserFactory, UserProfileFactory
+
+from openedx.core.djangolib.testing.utils import skip_unless_lms
+from django.test import TestCase, override_settings
+from openedx_filters.learning.enrollment import PreEnrollmentFilter
+
+
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
+
+from openedx_filters import PipelineStep
+
+class TestEnrollmentPipelineStep(PipelineStep):
+    """
+    Utility function used when getting steps for pipeline.
+    """
+
+    def do_filter(self, user, course_key, mode):
+        """Pipeline steps that changes mode to honor."""
+        if mode == "no-id-professional":
+            raise PreEnrollmentFilter.PreventEnrollment()
+        return {
+            "user": user, "course_key": course_key, "mode": "honor",
+        }
+
+
+@override_settings(
+        OPEN_EDX_FILTERS_CONFIG={
+            "org.openedx.learning.course.enrollment.started.v1": {
+                "pipeline": [
+                    "common.djangoapps.student.tests.tests_filters.TestEnrollmentPipelineStep",
+                ],
+                "fail_silently": False,
+            },
+        },
+    )
+@skip_unless_lms
+class EnrollmentFiltersTest(ModuleStoreTestCase):
+    """
+    Tests for the Open edX Events associated with the enrollment process through the enroll method.
+
+    This class guarantees that the following events are sent during the user's enrollment, with
+    the exact Data Attributes as the event definition stated:
+    """
+
+    def setUp(self):  # pylint: disable=arguments-differ
+        super().setUp()
+        self.course = CourseFactory.create()
+        self.user = UserFactory.create(
+            username="test",
+            email="test@example.com",
+            password="password",
+        )
+        self.user_profile = UserProfileFactory.create(user=self.user, name="Test Example")
+
+    def test_enrollment_filter_executed(self):
+        """
+        Test whether the student enrollment event is sent after the user's
+        enrollment process.
+
+        Expected result:
+            - COURSE_ENROLLMENT_CREATED is sent and received by the mocked receiver.
+            - The arguments that the receiver gets are the arguments sent by the event
+            except the metadata generated on the fly.
+        """
+        enrollment = CourseEnrollment.enroll(self.user, self.course.id, 'audit')
+
+        self.assertEqual('honor', enrollment.mode)
+
+    def test_enrollment_filter_prevent_enroll(self):
+        """
+        Test whether the student enrollment event is sent after the user's
+        enrollment process.
+
+        Expected result:
+            - COURSE_ENROLLMENT_CREATED is sent and received by the mocked receiver.
+            - The arguments that the receiver gets are the arguments sent by the event
+            except the metadata generated on the fly.
+        """
+        with self.assertRaises(PreEnrollmentFilter.PreventEnrollment):
+            CourseEnrollment.enroll(self.user, self.course.id, 'no-id-professional')
