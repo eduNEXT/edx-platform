26b8b8d8e3b22fd75d1dcc59a1078074cffa2567
Alejandro Cardenas <alecar.main@gmail.com>
2022-05-20 23:23:10 +0100

    feat: add lms setting to set mfe config cache (#262)
    Co-authored-by: María Fernanda Magallanes Z <maria.magallanes@edunext.co>

--- openedx/core/djangoapps/mfe_api/views.py
+++ openedx/core/djangoapps/mfe_api/views.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,6 +13,7 @@ class MFEConfigView(APIView):
     Provides an API endpoint for MFE config from site configurations.
     """
 
+    @method_decorator(cache_page(settings.MFE_CONFIG_API_CACHE_TIMEOUT))
     def get(self, request):
         """
         GET /api/mfe/v1/config
