6a5302969a0067bb776e07d64f573dfd4de22f71
María Fernanda Magallanes Z <maria.magallanes@edunext.co>
2022-05-24 09:01:10 -0400

    test: fix it and simplify it

--- openedx/core/djangoapps/mfe_api/tests/test_views.py
+++ openedx/core/djangoapps/mfe_api/tests/test_views.py
@@ -2,29 +2,27 @@
 Test the MFE API
 """
 
+from unittest.mock import patch
+
 from django.conf import settings
+from django.urls import reverse
 from rest_framework import status
 
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from openedx.core.lib.api.test_utils import ApiTestCase
 
 
-
 class MFEConfigTestCase(ApiTestCase):
     """
     Test the MFE API
     """
-
+    @patch.dict(settings.FEATURES, {'ENABLE_MFE_CONFIG_API': True})
     def setUp(self):
-        self.mfe_config_api_url = '/api/mfe/v1/config'
+        self.mfe_config_api_url = reverse('mfe_config_api')
         return super().setUp()
 
     def test_get_mfe_config(self):
         """Test the get mfe config from MFE API."""
-        response = self.client.get(self.mfe_config_api_url)
-        if settings.FEATURES.get('ENABLE_MFE_CONFIG_API'):
-            mfe_config = configuration_helpers.get_value('MFE_CONFIG',{})
-            response_json = self.get_json(self.mfe_config_api_url)
-            assert response_json == mfe_config
-        else:
-            assert status.HTTP_404_NOT_FOUND == response.status_code
+        mfe_config = configuration_helpers.get_value('MFE_CONFIG',{})
+        response_json = self.get_json(self.mfe_config_api_url)
+        assert response_json == mfe_config
