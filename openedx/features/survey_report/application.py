aad277e3ff2074e79d9927abd49e1aff423dc6c6
henrrypg <henrry.pulgarin@edunext.co>
2022-10-18 16:41:56 -0500

    docs: update docstrings

--- openedx/features/survey_report/application.py
+++ openedx/features/survey_report/application.py
@@ -1,6 +1,6 @@
 """
-Contains the logic for generating a survey report.
+Contains the logic for manage a survey report.
 """
 
 def generate_report() -> None:
-    """Generate a survey report for all courses in the system."""
+    """ Generate a report with relevant data."""
