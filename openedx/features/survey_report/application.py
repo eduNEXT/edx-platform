76d91b2e35b0c9e9a186e388b96b51eedb1bf085
henrrypg <henrry.pulgarin@edunext.co>
2022-10-19 14:51:14 -0500

    feat: add method to get currently learners

--- openedx/features/survey_report/application.py
+++ openedx/features/survey_report/application.py
@@ -2,10 +2,12 @@
 Contains the logic for manage a survey report.
 """
 
-from openedx.features.survey_report.queries import get_unique_courses_offered
+from openedx.features.survey_report.queries import get_unique_courses_offered, currently_learners
 
 def generate_report() -> None:
     """ Generate a report with relevant data."""
     courses_offered=get_unique_courses_offered()
+    learners=currently_learners()
 
-    print(courses_offered)
+    print(f"Unique courses offered: {courses_offered}")
+    print(f"Currently learners: {learners}")
