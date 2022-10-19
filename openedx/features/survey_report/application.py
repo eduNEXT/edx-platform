6afe5c8d4306bf8e952e2fe5a7d2f3e4f069baf8
henrrypg <henrry.pulgarin@edunext.co>
2022-10-19 14:43:32 -0500

    feat: add method to get unique courses offered

--- openedx/features/survey_report/application.py
+++ openedx/features/survey_report/application.py
@@ -2,5 +2,10 @@
 Contains the logic for manage a survey report.
 """
 
+from openedx.features.survey_report.queries import get_unique_courses_offered
+
 def generate_report() -> None:
     """ Generate a report with relevant data."""
+    courses_offered=get_unique_courses_offered()
+
+    print(courses_offered)
