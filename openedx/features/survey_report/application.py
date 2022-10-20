75253c7f98ef0c80f21706890e958226ff5e2658
henrrypg <henrry.pulgarin@edunext.co>
2022-10-20 08:16:02 -0500

    chore: delete print

--- openedx/features/survey_report/application.py
+++ openedx/features/survey_report/application.py
@@ -7,5 +7,3 @@ from openedx.features.survey_report.queries import get_unique_courses_offered
 def generate_report() -> None:
     """ Generate a report with relevant data."""
     courses_offered=get_unique_courses_offered()
-
-    print(courses_offered)
