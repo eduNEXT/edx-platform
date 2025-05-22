d2ea49ef38e5c227540f34571b0e9f04c52d0ef2
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2025-05-22 16:33:22 -0500

    feat: allows to apply filters in the course page based on a URL

--- lms/static/js/discovery/views/filter_bar.js
+++ lms/static/js/discovery/views/filter_bar.js
@@ -26,6 +26,55 @@
                 this.listenTo(this.collection, 'remove', this.hideIfEmpty);
                 this.listenTo(this.collection, 'add', this.addFilter);
                 this.listenTo(this.collection, 'reset', this.resetFilters);
+
+                // The following lines allows us to apply filters in the course page based on a URL.
+                this.addFilterByURL();
+                this.on('filter:updated', this.updateCourseDiscovery);
+            },
+
+            updateCourseDiscovery: function(filter) {
+                var courseDiscovery = new CourseDiscovery();
+                var filters = this.collection.map(function(model) {
+                    return {
+                        type: model.get('type'),
+                        query: model.get('query')
+                    };
+                });
+
+                if (filters.length === 0) return;
+
+                const payload = { filters };
+
+                courseDiscovery.fetch({
+                    data: JSON.stringify(payload),
+                    contentType: "application/json",
+                    type: "POST"
+                });
+            },
+
+            addFilterByURL: function() {
+                // Parse filter params from URL and add to collection if present
+                // Example URL: <domain>/courses?filtertype=org&filterquery=uah
+                const urlParams = new URLSearchParams(window.location.search);
+                const filterType = urlParams.get('filtertype');
+                const filterQuery = urlParams.get('filterquery');
+
+                if (!(filterType && filterQuery)) {
+                    return;
+                }
+
+                const existingFilter = this.collection.get(filterType);
+                if (existingFilter) {
+                    this.trigger('clearFilter', filterType);
+                }
+
+                const filter = new Filter({
+                    type: filterType,
+                    query: filterQuery,
+                    name: filterQuery
+                });
+                this.collection.add(filter);
+                this.trigger('filter:updated', filter);
             },
 
             render: function() {
