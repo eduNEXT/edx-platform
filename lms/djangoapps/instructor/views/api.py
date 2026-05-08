fcb07007709b15cf7389928ca99e55bf10a8a85d
Bryann Valderrama <bryann.valderrama@edunext.co>
2026-05-08 12:13:05 -0500

    fix: wrap enrollment sync process in a transaction to ensure atomicity

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -812,9 +812,10 @@ class StudentsUpdateEnrollmentView(DeveloperErrorViewMixin, APIView):
                     "total_students": len(identifiers),
                 }
 
-        return self._process_enrollment_sync(
-            request.user, course_key, action, identifiers, auto_enroll, email_students, reason, secure
-        )
+        with transaction.atomic():
+            return self._process_enrollment_sync(
+                request.user, course_key, action, identifiers, auto_enroll, email_students, reason, secure
+            )
 
     def _process_enrollment_sync(
         self,
